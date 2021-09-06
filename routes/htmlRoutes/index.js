4444e8f92a1264e597371ec64a44a08fecdb8016
Collin <collinhallett@gmail.com>
2021-09-06 14:45:41 -0500

    deleted commented code

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const path = require('path')
-// require('../../public/notes.html')
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'))
